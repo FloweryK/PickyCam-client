cb204de580fe9385a75d30b9b67d0e59719d35f0
Minsang Yu <minsang_yu2@tmax.co.kr>
2022-01-07 17:47:23 +0900

    [feat] uri setting logic updated

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import "react-native-reanimated";
 import React, { useEffect, useRef, useState } from "react";
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import {
   Camera,
   useCameraDevices,
@@ -11,21 +11,24 @@ import { runOnJS } from "react-native-reanimated";
 import FastImage from "react-native-fast-image";
 import { io } from "socket.io-client";
 
+const HOST = "10.0.2.2";
+const PORT = "8080";
+
 const App = () => {
   const [cameraType, setCameraType] = useState("front");
   const [image, setImage] = useState("");
   const [uri, setUri] = useState("");
+
+  const [isConnected, setIsConnected] = useState(false);
 
   const devices = useCameraDevices();
   const device = cameraType == "front" ? devices.front : devices.back;
 
   const socket = useRef(null);
 
-  const frameProcessor = useFrameProcessor((frame) => {
+  const frameProcessor = useFrameProcessor((data) => {
     "worklet";
-
-    const result = sampleFrame(frame);
-
+    const result = sampleFrame(data);
     runOnJS(setImage)(result.encoded);
   }, []);
 
@@ -34,35 +37,41 @@ const App = () => {
   };
 
   useEffect(() => {
-    socket.current = io("http://10.0.2.2:8080");
+    socket.current = io(`http://${HOST}:${PORT}`);
 
     socket.current.on("connect", () => {
       console.log("connected");
+      setIsConnected(true);
     });
 
     socket.current.on("disconnect", () => {
       console.log("disconnected");
+      setIsConnected(false);
     });
 
     socket.current.on("response", (data) => {
       console.log("received");
-      const frame = JSON.parse(data).frame;
 
-      if (typeof frame !== "undefined") {
-        console.log("frame exists");
+      const json = JSON.parse(data);
+      const frame = json.frame;
+
+      if (frame.length > 0) {
         setUri(`data:image/jpeg;base64,${frame.slice(2, frame.length - 1)}`);
       } else {
-        console.log("frame does not exists");
         setUri(`data:image/jpeg;base64,${image}`);
       }
     });
   }, []);
 
   // emit event
-  useEffect(() => {
-    console.log("sent");
-
-    socket.current.emit("request", { frame: image });
+  useEffect(async () => {
+    if (isConnected) {
+      console.log("request sent");
+      socket.current.emit("request", { frame: image });
+    } else {
+      console.log("request not sent");
+      setUri(`data:image/jpeg;base64,${image}`);
+    }
   }, [image]);
 
   if (device == null) {
@@ -73,7 +82,7 @@ const App = () => {
     <View>
       <Button title="toggle camera" onPress={toggleCameraType} />
       <Camera
-        style={{ width: 300, height: 400 }}
+        // style={{ width: 300, height: 400 }}
         device={device}
         isActive={true}
         frameProcessor={frameProcessor}
@@ -83,7 +92,5 @@ const App = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
